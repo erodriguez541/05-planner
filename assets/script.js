7f6e53e7ee7f26b1aeaa5e308dcbc2a3451b7b54
ErnieRodriguez <erod541@gmail.com>
2021-04-11 22:10:19 -0500

    boiler plate, psuedo code and today's date

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+
+//      Psuedo Code
+//Set up Variables 
+//-JQuery select DOM elements
+
+//1.Display Current Date in header
+//  Get current Date
+//  Format Current Date
+// Update DOM with Current Date
+//2. Render Time Blocks
+//    *Read any commitments from localstorage
+//    * create input filed
+//    * Create "SAVE" button
+//      Add CSS Classes for style
+            //Determine if timeblock is 'past', 'present' or 'future'
+//      Add text/value from localstorage
+//      Append to DOM
+//      Add to local variable 
+ //3. Event Handler for "SAVE"
+//      When a user enters text into field then user clicks *Matching* save button save the related input field into local storage
+//      Display notification/toast when the save is complete
+//
+//
+//
+//
+$('#currentDay').text(dayjs().format('MMM Do, YYYY'));
