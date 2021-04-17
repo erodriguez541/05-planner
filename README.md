9a014bc76dee0a2d7d946f34413f3a8f9c455524
ErnieRodriguez <erod541@gmail.com>
2021-04-17 09:54:21 -0500

    Slight chage to readme and updated past, present, future logic

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ Day Planner
 # Description
 This homework assignment needs to show a current day planner.  A user should be able to save their planed "events" into the local storage.  As time progresses throughout the day those passed events should change color's to indicate they are in the passed.  Future events should be left green.  A user should be able to update and change their events as well.
 
+# Usage
+
+A user could use this in order to help plan their day.  
 # Resources
 Utilized some bootstrap documentation 
 
--- assets/script.js
+++ assets/script.js
@@ -2,11 +2,12 @@
 //      Psuedo Code
 //Set up Variables 
 //-JQuery select DOM elements
-
 //1.Display Current Date in header
+$('#currentDay').text(dayjs().format('MMMM Do, YYYY'));
 //  Get current Date
-//  Format Current Date
-// Update DOM with Current Date
+var currentTime= dayjs();
+currentTime = currentTime.startOf("hour");
+var beforeTime = dayjs().startOf('day').add(9,"hours");
 //2. Render Time Blocks
 //    *Read any commitments from localstorage
 //    * create input filed
@@ -19,17 +20,23 @@
  //3. Event Handler for "SAVE"
 //      When a user enters text into field then user clicks *Matching* save button save the related input field into local storage
 //      Display notification/toast when the save is complete
-//
-//
-//
-//
-
-$('#currentDay').text(dayjs().format('MMMM Do, YYYY'));
-var currentTime= dayjs();
-currentTime = currentTime.startOf("hour");
-var beforeTime = dayjs().startOf('day').add(9,"hours")
 
 /// Save Entry to local storage
+
+function timeBlockColor(){
+    var hour = dayjs().hours();
+    $(".form-control").each(function(){
+        var currentHour = parseInt($(this).attr("id"));
+        if (currentHour> hour){
+            $(this).addclass("future");
+        } else if (currentHour === hour){
+            $(this).addclass("present");
+        } else{
+            $(this).addclass("past");
+        }
+    })
+    console.log(timeBlockColor)
+}
 $('.saveBtn').click(function(){
     event.preventDefault();
     var formValue = $(this).siblings(".form-control").val();
